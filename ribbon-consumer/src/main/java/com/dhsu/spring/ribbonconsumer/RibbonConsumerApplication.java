8dc486ab8d57b53f570241ca0e47b2debd0359cc
苏登辉 <dhsu2@iflytek.com>
2018-11-09 09:23:18 +0800

    ribbon-consumer消费者项目初始化

---
+++ ribbon-consumer/src/main/java/com/dhsu/spring/ribbonconsumer/RibbonConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.dhsu.spring.ribbonconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonConsumerApplication.class, args);
+	}
+}

81bd45758cc167221188a19100e77eac66645f3d
苏登辉 <dhsu2@iflytek.com>
2018-11-09 14:29:42 +0800

    增加consumer接口

---
+++ ribbon-consumer/src/main/java/com/ribbob/controller/ConsumerController.java
@@ -0,0 +1,28 @@
+package com.ribbob.controller;
+
+import javax.print.DocFlavor.STRING;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName： ConsumerController
+ * @Author: dhSu
+ * @Description: 实现/ribbon-consumer接口来调用HELLO-SERVICE服务
+ * @Date:Created in 2018年11月9日
+ */
+@RestController
+public class ConsumerController {
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@RequestMapping(value="/ribbon-consumer",method=RequestMethod.GET)
+	public String helloConsumer() {
+		return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+	}
+
+}
